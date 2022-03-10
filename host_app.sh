b9920b1d960d4ed0d7599209cdf9216f1fa28fc0
xris-owen <chukwudiokolo14@gmail.com>
2022-03-10 03:44:06 +0100

    Additional functions and comments were added.

--- host_app.sh
+++ host_app.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-#Hosting an e-commerce app on a debian system(Ubuntu)
+# 
+# --- This script automates the deployment of KodeKloud ecommerce application
 
-# --- Function to colour the output message
-# GREEN="\033[0;32m"
-# RED="\033[0;31m"
 NO_COLOR="\033[0m"
 
+####################################################
+# --- Prints a given message with a specified color
+# --- Arguments
+# ---     Color: GREEN OR RED   
+# ---     Message
+####################################################
 function print_message(){
     case $1 in
         "GREEN") COLOR="\033[0;32m" ;;
@@ -13,12 +17,16 @@ function print_message(){
         "*") COLOR="\033[0m"
     esac
 
-    echo -e "${COLOR} $2 ${NO_COLOR}"
-    sleep 1
-    
+    echo -e "\n${COLOR} $2 ${NO_COLOR}"
+    sleep 2 
 }
 
-# --- Check if service is active
+####################################################
+# --- Checks the status of a given service. Error and 
+#     exit if service not active
+# --- Arguments
+# ---     Service: apache2 or mariadb   
+####################################################
 function check_service_status(){
     is_service_active=$(systemctl is-active $1)
     if [ "$is_service_active" = "active" ]
@@ -29,6 +37,40 @@ function check_service_status(){
         exit 1
     fi
 }
+
+####################################################
+# --- Checks if a port is enabled in a firewalld rule
+# --- Arguments
+# ---     Port: Eg 3306
+####################################################
+function is_firewalld_rule_configured(){
+    firewalld_ports=$(sudo firewall-cmd --list-all --zone=public | grep ports)
+    if [[ $firewalld_ports = *$1* ]]
+    then
+        print_message "GREEN" "Port $1 is configured."
+    else
+        print_message "RED" "Port $1 not configured"
+        exit 1
+    fi
+}
+
+####################################################
+# --- Checks if an item is present in a given webpage
+# --- Arguments
+# ---     Webpage
+# ---     Item to be checked.
+####################################################
+function check_item(){
+    if [[ $1 = *$2* ]]
+    then
+        print_message "GREEN" "$2 is available"
+    else
+        print_message "RED" "$2 is not available"
+    fi
+}
+
+################################################# FUNCTION DEFINITION ENDS ######################################
+
 
 # --- Install FirewallD
 print_message "GREEN" "Installing and starting firewalld"
@@ -54,6 +96,9 @@ check_service_status mariadb
 print_message "GREEN" "Configuring firewall services for DB"
 sudo firewall-cmd --permanent --zone=public --add-port=3306/tcp;
 sudo firewall-cmd --reload
+
+# --- Check if port 3306 is properly configured
+is_firewalld_rule_configured 3306
 
 # --- Configure Database
 print_message "GREEN" "Creating DB and DB user"
@@ -88,6 +133,9 @@ sudo apt install -y apache2 php php-mysql
 sudo firewall-cmd --permanent --zone=public --add-port=80/tcp
 sudo firewall-cmd --reload
 
+# --- Check if port 80 is properly configured
+is_firewalld_rule_configured 80
+
 # --- Change DirectoryIndex index.html to DirectoryIndex index.php to make the php page the default page basically for httpd
 # --- sudo sed -i 's/index.html/index.php/g' /etc/httpd/conf/httpd.conf
 # --- This is already handled in apache2 >> /etc/apache2/mod-enabled/dir.conf
@@ -112,5 +160,9 @@ sudo sed -i 's/172.20.1.101/localhost/g' /var/www/html/index.php
 print_message "GREEN" "All set"
 # curl http://localhost
 
-# --- Git Token 
-# --- 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
+webpage=$(curl http://localhost)
+
+for item in Laptop Sugar VR Phone Biro
+do 
+   check_item "$webpage" $item 
+done
